fe3427ef3775f8d6d4d6b651288b9784504257ef
Artem Urvantsev <corpsezor@gmail.com>
2026-06-10 00:44:00 +0600

    Clean up project structure

---
+++ the_snake/__init__.py
@@ -0,0 +1,43 @@
+"""Модуль игры «Змейка»."""
+
+from .main import (
+    Apple,
+    BOARD_BACKGROUND_COLOR,
+    DOWN,
+    GRID_HEIGHT,
+    GRID_SIZE,
+    GRID_WIDTH,
+    GameObject,
+    LEFT,
+    RIGHT,
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+    Snake,
+    UP,
+    clock,
+    handle_keys,
+    main,
+    screen,
+)
+
+__all__ = [
+    'Apple',
+    'BOARD_BACKGROUND_COLOR',
+    'DOWN',
+    'GRID_HEIGHT',
+    'GRID_SIZE',
+    'GRID_WIDTH',
+    'GameObject',
+    'LEFT',
+    'RIGHT',
+    'SCREEN_HEIGHT',
+    'SCREEN_WIDTH',
+    'Snake',
+    'UP',
+    'clock',
+    'handle_keys',
+    'main',
+    'screen',
+]
+PY
+
